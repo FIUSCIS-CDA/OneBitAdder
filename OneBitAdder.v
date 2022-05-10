fcf0566106ffdcd7afa794999cefe98f41486b93
movingpictures83 <movingpictures83@gmail.com>
2022-05-10 09:17:54 -0400

    Regenerated Verilog, which changed parameter order Fixed testbench

--- OneBitAdder.v
+++ OneBitAdder.v
@@ -1,36 +1,36 @@
-// Copyright (C) 1991-2015 Altera Corporation. All rights reserved.
-// Your use of Altera Corporation's design tools, logic functions 
-// and other software and tools, and its AMPP partner logic 
+// Copyright (C) 2020  Intel Corporation. All rights reserved.
+// Your use of Intel Corporation's design tools, logic functions 
+// and other software and tools, and any partner logic 
 // functions, and any output files from any of the foregoing 
 // (including device programming or simulation files), and any 
 // associated documentation or information are expressly subject 
-// to the terms and conditions of the Altera Program License 
-// Subscription Agreement, the Altera Quartus II License Agreement,
-// the Altera MegaCore Function License Agreement, or other 
-// applicable license agreement, including, without limitation, 
-// that your use is for the sole purpose of programming logic 
-// devices manufactured by Altera and sold by Altera or its 
-// authorized distributors.  Please refer to the applicable 
-// agreement for further details.
+// to the terms and conditions of the Intel Program License 
+// Subscription Agreement, the Intel Quartus Prime License Agreement,
+// the Intel FPGA IP License Agreement, or other applicable license
+// agreement, including, without limitation, that your use is for
+// the sole purpose of programming logic devices manufactured by
+// Intel and sold by Intel or its authorized distributors.  Please
+// refer to the applicable agreement for further details, at
+// https://fpgasoftware.intel.com/eula.
 
-// PROGRAM		"Quartus II 64-Bit"
-// VERSION		"Version 15.0.0 Build 145 04/22/2015 SJ Web Edition"
-// CREATED		"Mon Feb  1 19:49:15 2021"
+// PROGRAM		"Quartus Prime"
+// VERSION		"Version 20.1.1 Build 720 11/11/2020 SJ Lite Edition"
+// CREATED		"Tue May 10 09:13:47 2022"
 
 module OneBitAdder(
 	ci,
 	a,
 	b,
-	co,
-	s
+	s,
+	co
 );
 
 
 input wire	ci;
 input wire	a;
 input wire	b;
-output wire	co;
 output wire	s;
+output wire	co;
 
 wire	______NOTa___AND___NOTb______OR___aANDb___;
 wire	______NOTb___ANDa___OR______NOTa___ANDb___;
